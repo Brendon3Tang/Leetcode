6e304f0692eb9684ddca9b4b8fc4f8343eb2b794
Brendon3Tang <kyle3tang@gmail.com>
2022-02-17 21:16:59 -0800

    2022/02/17 新增一种写法

--- 206. Reverse Linked List.cpp
+++ 206. Reverse Linked List.cpp
@@ -19,5 +19,18 @@ public:
         newNext->next = curHead;//newNext始终指向curHead的next Node,因此可以开始建立翻转链表
         curHead->next = NULL;//将旧链表的箭头全部消除
         return newHead;//递归的每层都要返回newHead
+
+        /*ListNode * cur, *newNext, *prev;
+        newNext = NULL;
+        prev = NULL;
+        cur = curHead;
+        while(cur != NULL)
+        {
+            newNext = cur->next;  //保存cur的下一个节点
+            cur->next = prev;//把cur的next指向prev,开始翻转链表
+            prev = cur;//保存cur给prev,在下一次循环中使用
+            cur = newNext;//把cur指向当前cur的下一个节点，进行下一个节点的翻转，当cur的下一个节点为NULL时，结束循环
+        }
+        return prev;//在最后一次循环中prev保存了cur进入到NULL之前的最后一个节点（旧链表的末尾节点），即新的head节点，return这个节点*/
     }
 };
