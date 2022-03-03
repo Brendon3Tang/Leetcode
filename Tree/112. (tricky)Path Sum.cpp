68d58044a466217553811ceec98c43444bc2aa50
Brendon3Tang <kyle3tang@gmail.com>
2022-03-02 21:57:40 -0800

    2022/03/02

---
+++ Tree/112. (tricky)Path Sum.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int count = 0;
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(root == NULL)
+            return false;
+        
+        count += root->val;
+        if(count == targetSum && root->left == NULL && root->right == NULL)
+            return true;
+        
+         if(hasPathSum(root->left,targetSum) == true || hasPathSum(root->right,targetSum) == true)
+             return true;
+        count -= root->val;
+        return false;
+    }
+    
+};
