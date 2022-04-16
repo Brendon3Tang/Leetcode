466dc8d425fe1f9097e7491ac128c6d463ea36f7
Long Tang <kyle3tang@gmail.com>
2022-04-16 13:55:42 -0700

    2022/-4/16新增背包问题解法

--- DP/70. Climbing Stairs.cpp
+++ DP/70. Climbing Stairs.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     int climbStairs(int n) {
         //1.base case
@@ -16,5 +16,23 @@ public:
             pre2 = sum;
         }
         return sum;
+    }
+};*/
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n+1,0);
+        dp[0] = 1;
+        
+        for(int i = 0; i <= n; i++)
+        {
+            for(int j = 1; j <= 2; j++)
+            {
+                if(i >= j)
+                    dp[i] += dp[i-j];
+            }
+        }
+        return dp[n];
     }
 };
