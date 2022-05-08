2f46b1b4d75473192be23e5c7292a65051847ef7
Long Tang <kyle3tang@gmail.com>
2022-05-07 20:00:52 -0700

    2022/05/07

--- Tree/94. Binary Tree Inorder Traversal.cpp
+++ Tree/94. Binary Tree Inorder Traversal.cpp
@@ -9,21 +9,45 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     vector<int> inorderTraversal(TreeNode* root) {
+//         vector<int> res;
+//         helper(root,res);
+//         return res;
+//     }
+    
+//     void helper(TreeNode* root, vector<int> &res)
+//     {
+//         if(root == nullptr)
+//             return;
+        
+//         helper(root->left, res);
+//         res.push_back(root->val);
+//         helper(root->right,res);
+//     }
+// };
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> res;
-        helper(root,res);
+        stack<TreeNode*> st;
+        TreeNode* cur = root;
+        while(cur != NULL || !st.empty())
+        {
+            if(cur != NULL)
+            {
+                st.push(cur);
+                cur = cur->left;
+            }
+            else
+            {
+                cur = st.top(); //返回null节点的上一节点
+                st.pop();
+                res.push_back(cur->val);
+                cur = cur->right;
+            }
+        }
         return res;
-    }
-    
-    void helper(TreeNode* root, vector<int> &res)
-    {
-        if(root == nullptr)
-            return;
-        
-        helper(root->left, res);
-        res.push_back(root->val);
-        helper(root->right,res);
     }
 };
