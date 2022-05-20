c2e3511ae8d1e29e8b87576f14b26043f926cecb
Long Tang <kyle3tang@gmail.com>
2022-05-20 16:28:32 -0700

    2022/05/20 新增自己的回溯

--- Tree/112. (tricky)Path Sum.cpp
+++ Tree/112. (tricky)Path Sum.cpp
@@ -9,21 +9,47 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     int count = 0;
+//     bool hasPathSum(TreeNode* root, int targetSum) {
+//         if(root == NULL)
+//             return false;
+        
+//         count += root->val;
+//         if(count == targetSum && root->left == NULL && root->right == NULL)
+//             return true;
+        
+//          if(hasPathSum(root->left,targetSum) == true || hasPathSum(root->right,targetSum) == true)
+//              return true;
+//         count -= root->val;
+//         return false;
+//     }
+    
+// };
+//自己的回溯算法
 class Solution {
+private:
+    bool flag = false;
+    void backtracking(TreeNode* root, int targetSum, int sum)
+    {
+        if(root == NULL)
+            return;
+        if(root->left == NULL && root->right == NULL)
+        {
+            if(sum + root->val == targetSum)
+                flag = true;
+            return;
+        }
+        
+        backtracking(root->left, targetSum, sum+root->val);
+        backtracking(root->right, targetSum, sum+root->val);
+    }
 public:
-    int count = 0;
     bool hasPathSum(TreeNode* root, int targetSum) {
         if(root == NULL)
             return false;
-        
-        count += root->val;
-        if(count == targetSum && root->left == NULL && root->right == NULL)
-            return true;
-        
-         if(hasPathSum(root->left,targetSum) == true || hasPathSum(root->right,targetSum) == true)
-             return true;
-        count -= root->val;
-        return false;
+        backtracking(root, targetSum, 0);
+        return flag;
     }
-    
 };
