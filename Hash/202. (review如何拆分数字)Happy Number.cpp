5fec49e737b974c699e9c1a07ad8ba06fb760eda
brendon3tang <kyle3tang@gmail.com>
2023-01-16 18:37:28 +0800

    day 6

--- Hash/202. (review如何拆分数字)Happy Number.cpp
+++ Hash/202. (review如何拆分数字)Happy Number.cpp
@@ -3,7 +3,7 @@ public:
     int getSum(int n) {
         int sum = 0;
         while (n) {
-            //例：19。 sum=9*9-> n = 19/10 = 1 -> sum = 1*1 -> n = 1/10 = 0 -> while结束。
+            //例：19。 sum=9*9, n = 19/10 = 1 -> sum = 1*1, n = 1/10 = 0 -> while结束, sum = 82。
             sum += (n % 10) * (n % 10);
             n /= 10;
         }
