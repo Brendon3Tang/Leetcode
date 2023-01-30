1f24e0e6aadd60caa5958b545e79250f31eab685
brendon3tang <kyle3tang@gmail.com>
2023-01-30 15:55:21 +0800

    day 14

--- Tree/104. Maximum Depth of Binary Tree.cpp
+++ Tree/104. Maximum Depth of Binary Tree.cpp
@@ -18,3 +18,27 @@ public:
         return max(maxDepth(root->left),maxDepth(root->right)) + 1;
     }
 };
+
+//层序遍历法
+/*
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root == NULL) return 0;
+        int depth = 0;
+        queue<TreeNode*> que;
+        que.push(root);
+        while(!que.empty()) {
+            int size = que.size();
+            depth++; // 记录深度
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = que.front();
+                que.pop();
+                if (node->left) que.push(node->left);
+                if (node->right) que.push(node->right);
+            }
+        }
+        return depth;
+    }
+};
+*/
